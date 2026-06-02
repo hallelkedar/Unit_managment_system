5481f539fdd0305019ce004107be49c12fc99274
Hallel <hallelkedar000@gmail.com>
2026-06-02 17:37:23 +0300

    add and fix crud logic handle

--- utils/io.py
+++ utils/io.py
@@ -1,68 +1,82 @@
 import os
 import json
 from logger_config import logger
-from helper import get_soldier_by_id, write_back_to_json
+from utils.helper import get_soldier_by_id, soldier_data_validation
 
-file_name = 'soldiers.json'
+FILE_NAME = 'soldiers.json'
 
-if not os.path.exists(file_name):
-    with open(file_name, 'w', encoding='utf-8') as f:
+if not os.path.exists(FILE_NAME):
+    with open(FILE_NAME, 'w', encoding='utf-8') as f:
         data = []
         json.dump(data, f, indent=2)
+
+
+def read_json():
+    with open(FILE_NAME, 'r', encoding='utf-8') as f:
+        return json.load(f)
     
+def write_back_to_json(data, file):
+    file.seek(0)
+    json.dump(data, file, indent=2)
+    file.truncate()
+
 def get_all_soldiers():
+
+    soldiers = read_json()
+    logger.info('Open soldiers json file, Return dict')
     
-    with open(file_name, 'r', encoding='utf-8') as f:
-        soldiers = json.load(f)
-        logger.info('Open soldiers json file, Get dict')
-        
-        if len(soldiers) == 0:
-            logger.warning('Soldiers list is empty.')
-        
-        return soldiers
+    if len(soldiers) == 0:
+        logger.warning('Soldiers list is empty.')
+    
+    return soldiers
 
 def get_soldier(soldier_id: int):
-    
-    with open(file_name, 'r', encoding='utf-8') as f:
-        soldiers = json.load(f)
         
-        return get_soldier_by_id(soldiers, soldier_id)
-    return None
+    soldiers = read_json()
+    return get_soldier_by_id(soldiers, soldier_id)
     
 def create_soldier(data: dict):
-    with open(file_name, 'r+', encoding='utf-8') as f:
-        logger.info('Try to create new soldier')
+    with open(FILE_NAME, 'r+', encoding='utf-8') as f:
         soldiers: list = json.load(f)
-        data['id'] = max(soldiers) + 1 if soldiers else 1
-        soldiers.append(data)
+        next_id = max(soldier['id'] for soldier in soldiers) + 1 if soldiers else 1
+        data['id'] = next_id
+        validation = soldier_data_validation(data)
+        if validation == data:
+            soldiers.append(data)
+
+            write_back_to_json(soldiers, f)
+            return True, next_id
+        return False, validation
         
-        write_back_to_json(soldiers, f)
-        logger.info('New soldier created.')
-        
-        return True
 
 def update_soldier(soldier_id: int, data: dict):
-    with open(file_name, 'r+', encoding='utf-8') as f:
-        logger.info('Try to update soldier')
+    with open(FILE_NAME, 'r+', encoding='utf-8') as f:
+        
         soldiers = json.load(f)
-        soldier:dict = get_soldier_by_id(soldiers, soldier_id)
-        if soldier:
+        soldier:dict | None = get_soldier_by_id(soldiers, soldier_id)
+        
+        if not soldier:
+            logger.error('ID was not found')
+            return False, None
+        
+        validation = soldier_data_validation(data)
+        if validation == data:
             soldier.update(data)
 
             write_back_to_json(soldiers, f)
-            logger.info(f'Soldier {soldier['id']} updated.')
-            return True
-        return False
+            return True, soldier['id']
+        
+        return False, validation
 
-def delete_soldier(soldier_id):
-    with open(file_name, 'r+', encoding='utf-8') as f:
-        logger.info('Try to delete soldier')
+def delete_soldier(soldier_id: int):
+    
+    with open(FILE_NAME, 'r+', encoding='utf-8') as f:
+        
         soldiers: list = json.load(f)
         soldier: dict = get_soldier_by_id(soldiers, soldier_id)
         if soldier:
             soldiers.remove(soldier)
 
             write_back_to_json(soldiers, f)
-            logger.info(f'Soldier {soldier['id']} deleted.')
             return True
         return False
