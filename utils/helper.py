e78a263baec5a31fad84a77ef5729a82a3de67e8
Hallel <hallelkedar000@gmail.com>
2026-06-02 16:55:05 +0300

    add data validation, fix get soldier by id

--- utils/helper.py
+++ utils/helper.py
@@ -1,12 +1,39 @@
-import json
+from pydantic import BaseModel, ValidationError
+from logger_config import logger
 
-def get_soldier_by_id(soldiers: dict, soldier_id: int):
+class SoldierProfile(BaseModel):
+    
+    id: int
+    full_name: str
+    personal_number: str
+    rank: str
+    role: str
+    unit: str
+    status: str
+
+        
+def soldier_data_validation(data: dict):
+    try:
+        SoldierProfile.model_validate(data)
+        return data
+    except ValidationError as e:
+        logger.warning(f'Soldier data validation stopped, error: {e}.')
+        
+        for error in e.errors():
+            field_name = str(error['loc'][0]) if error['loc'] else "global"
+            error_type = error['type']
+            error_msg = error['msg']
+
+            if error_type == 'missing':
+                logger.error(f'missing: {field_name} in data.')
+                return (error_type, field_name)
+            else:
+                logger.error(f'Invalid {field_name} in data, error: {error_msg}')
+                return (field_name, error_msg)
+         
+        
+def get_soldier_by_id(soldiers: list, soldier_id: int):
     for soldier in soldiers:
-            if soldier['id'] == soldier_id:
+            if soldier.get('id') == soldier_id:
                 return soldier
     return None
-
-def write_back_to_json(data, file):
-    file.seek(0)
-    json.dump(data, file, indent=2)
-    file.truncate()
