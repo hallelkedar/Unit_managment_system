77a229c1109e4908e22a044912ad7f7bc0d8fbd5
Hallel <hallelkedar000@gmail.com>
2026-06-03 09:07:44 +0300

    update and fix validation logic

--- utils/helper.py
+++ utils/helper.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel, ValidationError
 from logger_config import logger
 
-class SoldierProfile(BaseModel):
+class SoldierCreate(BaseModel):
     
-    id: int
     full_name: str
     personal_number: str
     rank: str
@@ -11,27 +10,10 @@ class SoldierProfile(BaseModel):
     unit: str
     status: str
 
-        
-def soldier_data_validation(data: dict):
-    try:
-        SoldierProfile.model_validate(data)
-        return data
-    except ValidationError as e:
-        logger.warning(f'Soldier data validation stopped, error: {e}.')
-        
-        for error in e.errors():
-            field_name = str(error['loc'][0]) if error['loc'] else "global"
-            error_type = error['type']
-            error_msg = error['msg']
-
-            if error_type == 'missing':
-                logger.error(f'missing: {field_name} in data.')
-                
-            else:
-                logger.error(f'Invalid {field_name} in data, error: {error_msg}')
-            return (error_type, field_name)
+class SoldierProfile(SoldierCreate):
+    id: int
          
-        
+    
 def get_soldier_by_id(soldiers: list, soldier_id: int):
     for soldier in soldiers:
             if soldier.get('id') == soldier_id:
